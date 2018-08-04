0fe725c0dbd5ce2c823aed7b82e6850c05d8ecb4
lyapandra <lyapandra.andriy.s@gmail.com>
2018-08-04 21:00:57 +0300

    connect to PostgreSQL (4-9%30)

---
+++ README.md
@@ -0,0 +1,4 @@
+used links:
+https://www.youtube.com/watch?v=fD7x8hd9yE4
+psql -U postgres -h localhost
+create user postgres with password '123';
--- src/main/java/letscode/sarafan/controller/MessageController.java
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,64 +1,70 @@
 package letscode.sarafan.controller;
 
-import letscode.sarafan.exceptions.NotFoundException;
+import letscode.sarafan.domain.Message;
+import letscode.sarafan.repo.MessageRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    private int counter = 4;
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    /*int counter = 4;
 
     private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
         add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
         add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
         add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
-    }};
+    }};*/
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return messages;
+    public List<Message> list() {
+        return messageRepo.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getOne(@PathVariable String id) {
-        return getMessage(id);
+    public Message getOne(@PathVariable("id") Message message/*String id*/) {
+        return message;
     }
 
-    private Map<String, String> getMessage(@PathVariable String id) {
+    /*private Map<String, String> getMessage(@PathVariable String id) {
         return messages.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
-    }
+    }*/
 
     @PostMapping
-    public Map<String, String> create(@RequestBody Map<String, String> message) {
-        message.put("id", String.valueOf(counter++));
-
-        messages.add(message);
-
-        return message;
+    public Message create(@RequestBody /*Map<String, String>*/ Message message) {
+        /*message.put("id", String.valueOf(counter++));
+        messages.add(message);*/
+        return messageRepo.save(message);
     }
 
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
-        Map<String, String> messageFromDb = getMessage(id);
-
+    public Message update(
+            @PathVariable ("id") /*String id*/ Message messageFromDb,
+            @RequestBody /*Map<String, String>*/ Message message) {
+/*        Map<String, String> messageFromDb = getMessage(id);
         messageFromDb.putAll(message);
-        messageFromDb.put("id", id);
-
-        return messageFromDb;
+        messageFromDb.put("id", id);*/
+        BeanUtils.copyProperties(message,messageFromDb,"id");
+        return messageRepo.save(messageFromDb);
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id) {
-        Map<String, String> message = getMessage(id);
-
-        messages.remove(message);
+    public void delete(@PathVariable("id") /*String id*/ Message message) {
+/*        Map<String, String> message = getMessage(id);
+        messages.remove(message);*/
+        messageRepo.delete(message);
     }
 }
